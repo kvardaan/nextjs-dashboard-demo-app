bb591968d7275433706320aacf411a7c3561c38e
kvardaan <vardaankhattar8907@gmail.com>
2024-06-06 09:24:21 +0530

    Streaming-1

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -16,12 +16,12 @@ const iconMap = {
 
 export default async function CardWrapper() {
   const {
-    totalPaidInvoices,
-    totalPendingInvoices,
     numberOfInvoices,
     numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
   } = await fetchCardData();
-
+  
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
